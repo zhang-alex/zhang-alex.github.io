1d69b9895bd411e7135c39e5b94e53c085df04da
Alex Zhang <az@Alexs-MacBook-Pro-2.local>
2022-02-25 11:29:07 -0800

    added welcome SVG animation to the home screen plus fade out to white

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-const swup = new Swup()
+const options = {
+  animationSelector: '[class*="swup-transition-"]',
+  linkSelector:
+    'a[href^="' +
+    window.location.origin +
+    '"]:not([data-no-swup]), a[href^="/"]:not([data-no-swup]), a[href^="#"]:not([data-no-swup])',
+
+};
+
+const swup = new Swup(options)
+
+frontWheel = document.getElementById("wheel");
+
+frontWheel.onclick = function () {
+  console.log("clicked");
+  let body = document.querySelector("body");
+  body.style.background = "#111111";
+}
+function backWheelClickHandler() {
+  let body = document.querySelector("body");
+  body.style.background = "#FFFFFF";
+}
