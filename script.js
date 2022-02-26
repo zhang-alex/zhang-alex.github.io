6f0a70fdf513dee57a83ce02d6982ec1a51c7a6a
Alex Zhang <az@Alexs-MacBook-Pro-2.local>
2022-02-25 17:26:35 -0800

    working logo bounces

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-const options = {
-  animationSelector: '[class*="swup-transition-"]',
-  linkSelector:
-    'a[href^="' +
-    window.location.origin +
-    '"]:not([data-no-swup]), a[href^="/"]:not([data-no-swup]), a[href^="#"]:not([data-no-swup])',
-};
-
-const swup = new Swup(options)
-
-frontWheel = document.getElementById("wheel");
-
-frontWheel.onclick = function () {
-  console.log("clicked");
-  let body = document.querySelector("body");
-  body.style.background = "#111111";
-}
-function backWheelClickHandler() {
-  let body = document.querySelector("body");
-  body.style.background = "#FFFFFF";
-}
+// const options = {
+//   animationSelector: '[class*="swup-transition-"]',
+//   linkSelector:
+//     'a[href^="' +
+//     window.location.origin +
+//     '"]:not([data-no-swup]), a[href^="/"]:not([data-no-swup]), a[href^="#"]:not([data-no-swup])',
+// };
+// 
+// const swup = new Swup(options)
+//
+// frontWheel = document.getElementById("wheel");
+//
+// frontWheel.onclick = function () {
+//   console.log("clicked");
+//   let body = document.querySelector("body");
+//   body.style.background = "#111111";
+// }
+// function backWheelClickHandler() {
+//   let body = document.querySelector("body");
+//   body.style.background = "#FFFFFF";
+// }
