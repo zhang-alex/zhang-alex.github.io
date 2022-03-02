77a504f6daa396485350db340543283152bf082b
Alex Zhang <alexzhang02030203@gmail.com>
2022-03-02 13:28:07 -0800

    added page transition for professional page

--- home.js
+++ home.js
@@ -1,6 +1,6 @@
 const main = () => {
   let computer = document.querySelector("#computer");
-  computer.onclick = () => window.location.href = "/professional.html";
+  computer.onclick = () => window.location.href = "/professional/professional.html";
 };
 
 main();
