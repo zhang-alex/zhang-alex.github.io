db0e6e7b6f9acf6b0ff8837c5f88ad0066d0feb1
Alex Zhang <alexzhang02030203@gmail.com>
2022-03-03 23:22:02 -0800

    made the following changes
    - uploaded new fig / home svg that combines the icons and levitators
      into one clickable icon
    - cleaned up both professional and home pages
    - removed javascript icon clicklisteners in favor of a href:xlink html
      tags for the svg components that are meant to be clickable
    - increased font size of introduction font on professional screen

--- home.js
+++ home.js
@@ -1,6 +1,16 @@
-const main = () => {
-  let computer = document.querySelector("#computer");
-  computer.onclick = () => window.location.href = "/professional/professional.html";
-};
+// overrode - placed a xlink:href tags inside of the svg to free up
+// compututational resources that would otherwise be assigned to the click
+// listeners
 
-main();
+// const main = () => {
+//   let computer = document.querySelector("#computer");
+//   computer.onclick = () => window.location.href = "/professional/professional.html";
+//
+//   let me = document.querySelector("#me");
+//   me.onclick = () => window.location.href = "/personal/personal.html";
+//
+//   let linkedinLev = document.querySelector("#linkedin-lev");
+//   linkedinLev.onclick = () => window.location.href = "/personal/personal.html";
+// };
+//
+// main();
