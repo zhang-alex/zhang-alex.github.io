30824b023168cd0b98bc8467270d05d567ad6e33
Alex Zhang <alexzhang02030203@gmail.com>
2022-03-03 23:27:15 -0800

    deleted some javascript from the script.js file and (effectively) deleted background from the home svg

--- script.js
+++ script.js
@@ -5,17 +5,5 @@
 //     window.location.origin +
 //     '"]:not([data-no-swup]), a[href^="/"]:not([data-no-swup]), a[href^="#"]:not([data-no-swup])',
 // };
-// 
+//
 // const swup = new Swup(options)
-//
-// frontWheel = document.getElementById("wheel");
-//
-// frontWheel.onclick = function () {
-//   console.log("clicked");
-//   let body = document.querySelector("body");
-//   body.style.background = "#111111";
-// }
-// function backWheelClickHandler() {
-//   let body = document.querySelector("body");
-//   body.style.background = "#FFFFFF";
-// }
