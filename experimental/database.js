fb17f7154889b7201c8db12f0d1b40f26e2776d3
zhang-alex <alexzhang02030203@gmail.com>
2023-02-19 16:45:29 -0800

    added mechanism in left column to generate any number of distinct random facts.

--- experimental/database.js
+++ experimental/database.js
@@ -1,7 +1,21 @@
 database = {
-    0: 'Kamala Harris',
-    1: 'Oprah Winfrey',
-    2: 'Michelle Obama',
-    3: 'Rosa Parks',
-    4: 'Marie Antoinette',
+    0: {name: 'Kamala Harris', question: 'Can You Name this Famous Political Figure?'},
+    1: {name: 'Oprah Winfrey', question: 'Can You Name this Famous TV shot host'},
+    2: {name: 'Michelle Obama', question: 'Can You Name this Famous Political Figure?'},
+    3: {name: 'Rosa Parks', question: 'Can You Name this Famous Political Figure?'},
+    4: {name: 'Marie Antoinette', question: 'Can You Name this Famous Historical Figure?'},
 }
+
+random_facts = [
+    "Flamingos turn pink from eating shrimp.",
+    "The Bee Hummingbird weighs less than a penny.",
+    "The Hawaiian alphabet has only 12 letters.",
+    "A ball of glass will bounce higher than a ball of rubber of the same size.",
+    "Termites eat through wood 2 times faster when listening to rock music.",
+    "The bullfrog is the only animal that never sleeps.",
+    "Elephants are capable of swimming 20 miles a day.",
+    "Elephants are the only mammals that can't jump.",
+    "Cats can hear ultrasound.",
+]
+
+
--- experimental/experimental.js
+++ experimental/experimental.js
@@ -1,14 +1,31 @@
 const K_ANSWERS = 4;
+const K_RANDOM_FACTS = 4;
 const pageNum = 0;
 
+let correctAnswer = Math.floor(Math.random() * K_ANSWERS);
+
 function main() {
-    console.log(createListOfNRandInts(3, 20));
     render();
 }
 
 function render() {
+    renderLeft();
+    renderMid();
+}
+
+function renderLeft() {
+    renderNRandomFacts(K_RANDOM_FACTS);
+}
+
+function renderMid() {
+    renderMidTitle();
     renderImage();
     renderAnswerButtons();
+}
+
+function renderMidTitle() {
+    let title = createMyElement('h2', {class: "mid-col-title"}, database[correctAnswer].question);
+    document.querySelector(".mid-col-content").prepend(title);
 }
 
 function renderAnswerButtons() {
@@ -18,18 +35,31 @@ function renderAnswerButtons() {
 
 function renderImage() {
     let i = 0;
-    const myImage = createMyElement('img', {src: `images/${i}.jpg`, height: '700px'})
+    const myImage = createMyElement('img', {src: `images/${i}.jpg`, height: '400px'})
     let container = document.querySelector(".subject-icon-container");
     container.appendChild(myImage);
 }
 
+function renderNRandomFacts(n) {
+    let random_facts = getNRandomFacts(n);
+    for (let i = 0; i < n; i++) {
+        // Find the element with ID "facts-below"
+        const factsBelow = document.getElementById("facts-below");
+
+        // Create a new div element with the class "fact-container"
+        const factContainer = createMyElement("div", {class: "fact-container"}, random_facts[i]);
+
+        // Insert the new element below the "facts-below" element
+        factsBelow.insertAdjacentElement("afterend", factContainer);
+    }
+}
+
 function createAnswerButtons() {
-    let correctAnswer = Math.floor(Math.random() * K_ANSWERS);
     let otherAnswers = createListOfNRandInts(K_ANSWERS - 1, Object.keys(database).length, correctAnswer);
     let buttonArray = [];
 
     for (let i = 0; i < K_ANSWERS; i++) {
-        let text = (i == correctAnswer) ? database[pageNum] : database[otherAnswers.pop()];
+        let text = (i === correctAnswer) ? database[correctAnswer].name : database[otherAnswers.pop()].name;
         buttonArray[i] = createMyElement('button', {class: "button", id: `button${i}`}, [text]);
     }
 
@@ -37,20 +67,38 @@ function createAnswerButtons() {
 }
 
 function createListOfNRandInts(n, upperLimitExcl, skip) {
+    if (!skip) {
+        skip = []
+    }
+    if (!(skip instanceof Array)) {
+        skip = [skip]
+    }
+
     let res = [];
 
     while (res.length < n) {
         let candidate = Math.floor(Math.random() * upperLimitExcl);
-        if (!res.includes((candidate)) && candidate !== skip) {
-            res.push(candidate);
+        if (res.includes((candidate)) || skip.includes(candidate)) {
+            continue;
         }
+        res.push(candidate);
     }
 
     return res;
 }
 
+function getNRandomFacts(n) {
+    let res = createListOfNRandInts(n, random_facts.length, );
+    res.forEach((elem, idx) => res[idx] = random_facts[elem]);
+    return res;
+}
+
 
 createMyElement = (type, attributes, children) => {
+    if (!(children instanceof Array)) {
+        children = [children];
+    }
+
     let myElement = document.createElement(type);
 
     for (let a in attributes) {
