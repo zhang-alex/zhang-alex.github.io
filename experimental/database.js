a4e51a99d4fa3f3b9f7fb5367e012663ecab3120
zhang-alex <alexzhang02030203@gmail.com>
2023-02-21 17:02:59 -0800

    cleared out all body HTML in all files to be added in JS (in a not very clean way but it will do). also implemented page-switching functionality.

--- experimental/database.js
+++ experimental/database.js
@@ -1,9 +1,54 @@
 database = {
-    0: {name: 'Kamala Harris', question: 'Can You Name this Famous Political Figure?'},
-    1: {name: 'Oprah Winfrey', question: 'Can You Name this Famous TV shot host'},
-    2: {name: 'Michelle Obama', question: 'Can You Name this Famous Political Figure?'},
-    3: {name: 'Rosa Parks', question: 'Can You Name this Famous Political Figure?'},
-    4: {name: 'Marie Antoinette', question: 'Can You Name this Famous Historical Figure?'},
+    0: {
+        name: 'Kamala Harris',
+        question: 'Can You Name this Famous Political Figure?',
+        description: 'Kamala Harris is an American politician and attorney who made history as the first female, first Black, and first South Asian vice president of the United States.'
+    },
+    1: {
+        name: 'Oprah Winfrey',
+        question: 'Can You Name this Famous TV show host?',
+        description: 'Oprah Winfrey is an American media executive, actress, talk show host, television producer, and philanthropist. She is best known for her talk show The Oprah Winfrey Show, which was the highest-rated television program of its kind in history.'
+    },
+    2: {
+        name: 'Michelle Obama',
+        question: 'Can You Name this Famous Political Figure?',
+        description: 'Michelle Obama is an American lawyer, university administrator, and writer who served as the first lady of the United States from 2009 to 2017. She is an advocate for education, healthy living, and poverty awareness.'
+    },
+    3: {
+        name: 'Rosa Parks',
+        question: 'Can You Name this Famous Political Figure?',
+        description: 'Rosa Parks was an American civil rights activist who is known as the "Mother of the Modern-Day Civil Rights Movement." She refused to give up her seat to a white passenger on a Montgomery, Alabama, bus, which helped spark the Montgomery bus boycott and eventually led to the desegregation of public transportation.'
+    },
+    4: {
+        name: 'Marie Antoinette',
+        question: 'Can You Name this Famous Historical Figure?',
+        description: 'Marie Antoinette was the last queen of France before the French Revolution. She is best known for her extravagant spending and her alleged statement "Let them eat cake," which came to symbolize the wealth and excess of the French aristocracy.'
+    },
+    5: {
+        name: 'Frida Kahlo',
+        question: 'Can You Name this Famous Artist?',
+        description: 'Frida Kahlo was a Mexican artist who is best known for her self-portraits and surrealist works. She is considered one of the most important female artists of the 20th century.'
+    },
+    6: {
+        name: 'Amelia Earhart',
+        question: 'Can You Name this Famous Aviator?',
+        description: 'Amelia Earhart was an American aviator who set many aviation records and became the first woman to fly solo across the Atlantic Ocean.'
+    },
+    7: {
+        name: 'Harriet Tubman',
+        question: 'Can You Name this Famous Abolitionist?',
+        description: 'Harriet Tubman was an American abolitionist who escaped from slavery and became a "conductor" on the Underground Railroad, leading hundreds of slaves to freedom.'
+    },
+    8: {
+        name: 'Coco Chanel',
+        question: 'Can You Name this Famous Fashion Designer?',
+        description: 'Coco Chanel was a French fashion designer who revolutionized women\'s fashion in the early 20th century. She is best known for popularizing the "little black dress" and the Chanel suit.'
+    },
+    9: {
+        name: 'Ada Lovelace',
+        question: 'Can You Name this Famous Computer Programmer?',
+        description: 'Ada Lovelace was an English mathematician and writer who is best known for her work on Charles Babbage\'s proposed mechanical general-purpose computer, the Analytical Engine. She is considered the first computer programmer.'
+    },
 }
 
 random_facts = [
--- experimental/experimental.js
+++ experimental/experimental.js
@@ -1,16 +1,72 @@
 const K_ANSWERS = 4;
 const K_RANDOM_FACTS = 4;
-const pageNum = 0;
-
+let pageNum = 0;
 let correctAnswer = Math.floor(Math.random() * K_ANSWERS);
+const K_TOTAL_PAGES = 5;
 
-function main() {
+function main(n) {
+    pageNum = n;
     render();
 }
 
 function render() {
+    renderHeader();
+    renderBody();
+    renderFooter();
+}
+
+function renderHeader() {
+    let header_text = createMyElement('h1', null, "IQMaster");
+    let header = createMyElement('header', null, header_text);
+    let body = document.querySelector('body')
+    body.appendChild(header);
+    body.innerHTML +=
+`
+<main>
+    <div id="left-col" class="col">
+        <h2 class="left-col-title">Did you know?</h2>
+        <hr id="facts-below">
+    </div>
+    <div id="mid-col" class="col">
+        <div class="mid-col-content">
+            <div class="subject-icon-container">
+            </div>
+            <div class="ad">
+                <!-- Google AdSense Ad -->
+            </div>
+            <div class="ad" id="buttons-before">
+                <!-- Google AdSense Ad -->
+            </div>
+            <div class="ad">
+                <!-- Google AdSense Ad -->
+            </div>
+            <button id="continue">Continue</button>
+        </div>
+    </div>
+    <div id="right-col" class="col">
+        <div class="ad">
+            <!-- Google AdSense Ad -->
+        </div>
+        <div class="ad">
+            <!-- Google AdSense Ad -->
+        </div>
+    </div>
+</main>
+<footer>
+    <div class="ad">
+        <!-- Google AdSense Ad -->
+    </div>
+</footer>
+`
+}
+
+function renderBody() {
     renderLeft();
     renderMid();
+}
+
+function renderFooter() {
+
 }
 
 function renderLeft() {
@@ -18,24 +74,25 @@ function renderLeft() {
 }
 
 function renderMid() {
+    document.querySelector("#continue").addEventListener('click', nextPage);
     renderMidTitle();
     renderImage();
     renderAnswerButtons();
 }
 
 function renderMidTitle() {
-    let title = createMyElement('h2', {class: "mid-col-title"}, database[correctAnswer].question);
+    let title = createMyElement('h2', {class: "mid-col-title"}, database[pageNum].question);
     document.querySelector(".mid-col-content").prepend(title);
 }
 
 function renderAnswerButtons() {
     let container = document.querySelector(".mid-col-content");
-    container.insertBefore(createAnswerButtons(), document.querySelector("#buttons-before"));
+    let button_group = createAnswerButtons();
+    container.insertBefore(button_group, document.querySelector("#buttons-before"));
 }
 
 function renderImage() {
-    let i = 0;
-    const myImage = createMyElement('img', {src: `images/${i}.jpg`, height: '400px'})
+    const myImage = createMyElement('img', {src: `images/${pageNum}.jpg`, height: '400px'})
     let container = document.querySelector(".subject-icon-container");
     container.appendChild(myImage);
 }
@@ -55,23 +112,51 @@ function renderNRandomFacts(n) {
 }
 
 function createAnswerButtons() {
-    let otherAnswers = createListOfNRandInts(K_ANSWERS - 1, Object.keys(database).length, correctAnswer);
+    let otherAnswers = createListOfNRandInts(K_ANSWERS - 1, Object.keys(database).length, pageNum);
     let buttonArray = [];
+    // console.log(otherAnswers[0]);
+    // console.log(otherAnswers[1]);
+    // console.log(otherAnswers[2]);
+    // console.log(pageNum);
+    // console.log('-');
 
     for (let i = 0; i < K_ANSWERS; i++) {
-        let text = (i === correctAnswer) ? database[correctAnswer].name : database[otherAnswers.pop()].name;
+        let text;
+        if (i === correctAnswer) {
+            text = database[pageNum].name;
+        } else {
+            text = database[otherAnswers.pop()].name;
+        }
         buttonArray[i] = createMyElement('button', {class: "button", id: `button${i}`}, [text]);
+        buttonArray[i].addEventListener("click", answerButtonClick)
     }
 
     return createMyElement('div', {class: "btn-group"}, buttonArray);
 }
 
+function answerButtonClick() {
+    let button_group = document.querySelector(".btn-group");
+    for (let child = button_group.firstChild; child !== null; child = child.nextSibling) {
+        if (child.textContent === database[pageNum].name) {
+            child.style.backgroundColor = "green";
+        } else {
+            child.style.backgroundColor = "red";
+        }
+    }
+}
+
+function nextPage() {
+    window.location.href = `${(pageNum+1) % K_TOTAL_PAGES}.html`;
+}
+
 function createListOfNRandInts(n, upperLimitExcl, skip) {
-    if (!skip) {
+    console.log(n, upperLimitExcl, skip);
+    if (typeof skip === 'undefined') {
         skip = []
     }
     if (!(skip instanceof Array)) {
-        skip = [skip]
+        skip = [skip];
+        console.log("YAYAYAYAYA");
     }
 
     let res = [];
@@ -81,6 +166,10 @@ function createListOfNRandInts(n, upperLimitExcl, skip) {
         if (res.includes((candidate)) || skip.includes(candidate)) {
             continue;
         }
+        console.log(candidate);
+        console.log(skip[0]);
+        console.log(skip.includes(candidate));
+        console.log(skip.includes(0));
         res.push(candidate);
     }
 
